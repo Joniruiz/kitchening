2afecde2dc01fa546d53c5b47a323526a091740f
Joniruiz <ruiz.jonathan2018@gmail.com>
2021-06-28 11:28:13 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+/* Middleware*/
+app.use(express.static('public'))
+
+/* Routes*/
+app.get("/" , (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+
+
+app.listen(port , ()=> console.log(`Servidor levantado en el puerto ${port}\n http://localhost:${port}`))
+
